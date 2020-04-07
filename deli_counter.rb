7d012b995f021a27d423c306c615e594935e3756
Jack Cooper <jackcooper1245@gmail.com>
2020-04-07 15:42:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(katz_deli)
   katz_deli.each_with_index do |name, index|
-   puts "The line is currently: "#{index}. #{name}
+   puts "The line is currently: #{index}. #{name}"
   else katz_deli.length <= 0
   puts "The line is currently empty"
 end
