5a2aa48a7cc1b3e7845d2ea9eefca4216de50d2f
Jack Cooper <jackcooper1245@gmail.com>
2020-04-07 15:56:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,15 @@
 katz_deli = []
 
-def line(katz_deli)
-if katz_deli.length == 0 
-  puts "The line is currently empty."
-else
-  queue = []
-  counter = 1
-  katz_deli.each do |name|
-    queue.push("#{counter}. #{name}")
-    counter += 1
-    puts "The line is currently: #{queue.join(" ")}" 
-  end
+def line(array) 
+  if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
   end
 end
